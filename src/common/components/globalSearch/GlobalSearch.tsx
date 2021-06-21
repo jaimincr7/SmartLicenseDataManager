56830b20ab4fde5057e0443e4ab2fe3b17bb7fa5
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-06-21 16:39:32 +0530

    bind dependent dropdown in GlobalSearch

--- src/common/components/globalSearch/GlobalSearch.tsx
+++ src/common/components/globalSearch/GlobalSearch.tsx
@@ -1,32 +1,84 @@
-import { Select } from 'antd';
+import { Form, Select } from 'antd';
+import { useEffect } from 'react';
+import { ILookup } from '../../../services/common/common.model';
+import { useAppDispatch, useAppSelector } from '../../../store/app.hooks';
+import { getBULookup, getCompanyLookup, getTenantLookup } from '../../../store/common/common.action';
+import { commonSelector } from '../../../store/common/common.reducer';
 
 const GlobalSearch: React.FC = () => {
+  const commonLookups = useAppSelector(commonSelector);
+  const dispatch = useAppDispatch();
+
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  const handleTenantChange = (tenantId: number) => {
+    form.setFieldsValue({ tenant: tenantId, company: null, bu: null });
+    dispatch(getCompanyLookup(tenantId));
+  };
+
+  const handleCompanyChange = (companyId: number) => {
+    form.setFieldsValue({ company: companyId, bu: null });
+    dispatch(getBULookup(companyId));
+  };
+
+  const handleBUChange = (buId: number) => {
+    form.setFieldsValue({ bu: buId });
+  };
+
+  useEffect(() => {
+    dispatch(getTenantLookup());  
+  }, [dispatch]);
+
   return (
     <>
-      <Select
-        placeholder="Filter by Tenant"
-        suffixIcon={<img src={`${process.env.PUBLIC_URL}/assets/images/ic-down.svg`} alt="" />}
-      >
-        <Select.Option value="1">John Smith</Select.Option>
-        <Select.Option value="2">John Smith</Select.Option>
-        <Select.Option value="3">John Smith</Select.Option>
-      </Select>
-      <Select
-        placeholder="Filter by Company"
-        suffixIcon={<img src={`${process.env.PUBLIC_URL}/assets/images/ic-down.svg`} alt="" />}
-      >
-        <Select.Option value="1">John Smith</Select.Option>
-        <Select.Option value="2">John Smith</Select.Option>
-        <Select.Option value="3">John Smith</Select.Option>
-      </Select>
-      <Select
-        placeholder="Filter by BU"
-        suffixIcon={<img src={`${process.env.PUBLIC_URL}/assets/images/ic-down.svg`} alt="" />}
-      >
-        <Select.Option value="1">John Smith</Select.Option>
-        <Select.Option value="2">John Smith</Select.Option>
-        <Select.Option value="3">John Smith</Select.Option>
-      </Select>
+      <Form form={form} name="horizontal_filter" layout="inline" onFinish={onFinish}>
+        <Form.Item name="tenant" className="mr-1">
+          <Select
+            placeholder="Filter by Tenant"
+            onChange={handleTenantChange}
+            suffixIcon={<img src={`${process.env.PUBLIC_URL}/assets/images/ic-down.svg`} alt="" />}
+            allowClear
+          >
+            {commonLookups.tenantLookup.data.map((option: ILookup) => (
+              <Select.Option key={option.id} value={option.id}>
+                {option.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item name="company" className="mr-1">
+          <Select
+            placeholder="Filter by Company"
+            onChange={handleCompanyChange}
+            suffixIcon={<img src={`${process.env.PUBLIC_URL}/assets/images/ic-down.svg`} alt="" />}
+            allowClear
+          >
+            {commonLookups.companyLookup.data.map((option: ILookup) => (
+              <Select.Option key={option.id} value={option.id}>
+                {option.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item name="bu" className="m-0">
+          <Select
+            placeholder="Filter by BU"
+            onChange={handleBUChange}
+            suffixIcon={<img src={`${process.env.PUBLIC_URL}/assets/images/ic-down.svg`} alt="" />}
+            allowClear
+          >
+            {commonLookups.buLookup.data.map((option: ILookup) => (
+              <Select.Option key={option.id} value={option.id}>
+                {option.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+      </Form>
     </>
   );
 };
