20ab46d892f5fd68112a6abf0adcc2103c7cdd30
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-07-02 18:17:05 +0530

    Set new design title drodown in Update from Excel

--- src/pages/Home/UploadExcel.tsx
+++ src/pages/Home/UploadExcel.tsx
@@ -1,7 +1,58 @@
-import { Select, Row, Col, Button } from 'antd';
+import { Select, Row, Col, Button, Form, Upload } from 'antd';
+import { Messages } from '../../common/constants/messages';
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../store/app.hooks';
+import { bulkInsert, getExcelColumns } from '../../store/sqlServer/sqlServer.action';
+import {
+  clearExcelColumns,
+  clearSqlServerMessages,
+  sqlServerSelector,
+} from '../../store/sqlServer/sqlServer.reducer';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const validateMessages = {
+  required: Messages.FIELD_REQUIRED,
+};
+
 const { Option } = Select;
 
 function UploadExcel() {
+   const sqlServers = useAppSelector(sqlServerSelector);
+  const dispatch = useAppDispatch();
+  const history = useHistory();
+
+  const [form] = Form.useForm();
+
+  const [defaultFile, setDefaultFile] = useState(null);
+  const [excelColumns, setExcelColumns] = useState(null);
+  const [tableColumns, setTableColumns] = useState([]);
+
+  const uploadFile = async (options) => {
+    const { onSuccess, onError, file } = options;
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('table_name', 'sql server');
+    try {
+      dispatch(getExcelColumns(formData));
+      onSuccess('Ok');
+    } catch (err) {
+      onError({ err });
+    }
+  };
+
+   const handleOnChange = ({ file }) => {
+    if (file.status === 'removed') {
+      dispatch(clearExcelColumns());
+      setDefaultFile(null);
+      setExcelColumns(null);
+      setTableColumns([]);
+    } else if (file.status === 'done') {
+      setDefaultFile(file);
+    }
+  };
+
   return (
     <div className="update-excel-page">
       <div className="title-block">
@@ -9,31 +60,77 @@ function UploadExcel() {
       </div>
       <div className="main-card">
         <div className="input-btns-title">
-          <div className="btns-block f-wrap">
-            <Button
-              type="primary"
-              className="btn-icon"
-              icon={
-                <em className="anticon">
-                  <img src={`${process.env.PUBLIC_URL}/assets/images/ic-upload.svg`} alt="" />
-                </em>
-              }
-            >
-              Upload Excel
-            </Button>
-            <div className="upload-file-path">
-              <em className="d-flex">
-                <img
-                  src={`${process.env.PUBLIC_URL}/assets/images/ic-baseline-attachment.svg`}
-                  alt=""
-                />
-              </em>
-              <span className="file-name">xyz.xls</span>
-              <Button className="action-btn p-0">
-                <img src={`${process.env.PUBLIC_URL}/assets/images/ic-delete.svg`} alt="" />
-              </Button>
+          
+       <Row gutter={[30, 20]} className="align-item-start">
+          <Col xs={24} md={8}>
+           
+            <label className="label w-100">Upload Excel</label>
+                 <div className="upload-file">
+              <Upload
+                accept=".xls,.xlsx"
+                customRequest={uploadFile}
+                onChange={handleOnChange}
+                defaultFileList={defaultFile}
+                maxCount={1}
+                showUploadList={{
+                  showRemoveIcon: true,
+                  removeIcon: (
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/ic-delete.svg`} alt="" />
+                  ),
+                }}
+              >
+                <Button
+                  type="primary"
+                  className="btn-icon"
+                  icon={
+                    <em className="anticon">
+                      <img src={`${process.env.PUBLIC_URL}/assets/images/ic-upload.svg`} alt="" />
+                    </em>
+                  }
+                >
+                  Upload Excel
+                </Button>
+              </Upload>
             </div>
-          </div>
+          </Col>
+            <Col xs={24} md={8}>
+              <div className="form-group m-0">
+                    <Form.Item
+                    className="m-0"
+                      >
+                <label className="label">Event Trigger Type</label>
+                <Select
+                  suffixIcon={
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/ic-down.svg`} alt="" />
+                  }
+                >
+                  <Option value="1">John Smith</Option>
+                  <Option value="2">John Smith</Option>
+                  <Option value="3">John Smith</Option>
+                </Select>
+                </Form.Item>
+              </div>
+            </Col>
+            <Col xs={24} md={8}>
+              <div className="form-group m-0">
+                <Form.Item
+                 className="m-0"
+                      >
+                <label className="label">Contract Type</label>
+                <Select
+                  suffixIcon={
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/ic-down.svg`} alt="" />
+                  }
+                >
+                  <Option value="1">John Smith</Option>
+                  <Option value="2">John Smith</Option>
+                  <Option value="3">John Smith</Option>
+                </Select>
+                </Form.Item>
+              </div>
+            </Col>
+            </Row>
+      
         </div>
         <Row gutter={[30, 0]}>
           <Col xs={24} md={12} lg={12} xl={8}>
