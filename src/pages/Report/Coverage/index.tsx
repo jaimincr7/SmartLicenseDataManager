a78d934b5efd3e5c634fb2f8a8e9347ef7d712ed
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-07-15 16:57:20 +0530

    Added report page

--- src/pages/Report/Coverage/index.tsx
+++ src/pages/Report/Coverage/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import authService from '../../../services/auth/auth.service';
 
 const ReportCoverage: React.FC = () => {
 
+    const [accessToken, setAccessToken] = React.useState<string>('')
 
-  return (
-    <div className="sqlServer">
-      <div className="title-block">
-        <h4 className="p-0">Coverage Report</h4>
-      </div>
-      <div className="main-card">
-          <p>Report</p>
-      </div>
-    </div>
-  );
+    React.useEffect(() => {
+        authService.getAuthToken().then((a) => {
+            setAccessToken(a as string);
+        });
+    }, [])
+
+    return (
+        <div className="sqlServer">
+            <div className="title-block">
+                <h4 className="p-0">Coverage Report</h4>
+            </div>
+            <div className="main-card">
+                <p>Report</p>
+            </div>
+        </div>
+    );
 };
 
 export default ReportCoverage;
