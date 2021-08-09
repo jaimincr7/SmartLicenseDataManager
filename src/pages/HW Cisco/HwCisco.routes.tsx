df07160628b88b2a855102011f1297a452e520ef
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-08-09 13:18:06 +0530

    committing updated code for cisco site matrix crud

--- src/pages/HW Cisco/HwCisco.routes.tsx
+++ src/pages/HW Cisco/HwCisco.routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
-// import CiscoSiteMatrix from './CiscoSiteMatrix';
-// import ability from '../../common/ability';
-// import { Action, Page } from '../../common/constants/pageAction';
+import ability from '../../common/ability';
+import { Action, Page } from '../../common/constants/pageAction';
+import CiscoSiteMatrix from './CiscoSiteMatrix';
 
 const HwCiscoRoutes: React.FC = () => {
   const match = useRouteMatch();
@@ -11,12 +11,12 @@ const HwCiscoRoutes: React.FC = () => {
     <div className="sqlServer">
       <Switch>
         {/* Site Matrix */}
-        {/* {ability.can(Action.View, Page.HwCiscoSiteMatrix) && (
+        {ability.can(Action.View, Page.HwCiscoSiteMatrix) && (
           <Route exact path={`${match.path}/cisco-site-matrix/:id`} component={CiscoSiteMatrix} />
         )}
         {ability.can(Action.View, Page.HwCiscoSiteMatrix) && (
           <Route exact path={`${match.path}/cisco-site-matrix`} component={CiscoSiteMatrix} />
-        )} */}
+        )}
 
         {/* keep least always */}
         <Route path={`${match.path}/*`}>
