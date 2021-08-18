b4bfb3d2587740ccd9c72c1a6aef19a3b212ec56
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-08-18 10:30:18 +0530

    Resolved CMS route issue.

--- src/pages/CMS/CMS.routes.tsx
+++ src/pages/CMS/CMS.routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 import ability from '../../common/ability';
 import { Action, Page } from '../../common/constants/pageAction';
+import CategoryExtended from './CategoryExtended';
 import CmsCategory from './CmsCategory';
 
 const HwCiscoRoutes: React.FC = () => {
@@ -16,6 +17,8 @@ const HwCiscoRoutes: React.FC = () => {
         )}
         {ability.can(Action.View, Page.CmsCategory) && (
           <Route exact path={`${match.path}/cms-category`} component={CmsCategory} />
+        )}
+
         {/* Category Extended */}
         {ability.can(Action.View, Page.CmsCategoryExtended) && (
           <Route
@@ -27,7 +30,7 @@ const HwCiscoRoutes: React.FC = () => {
         {ability.can(Action.View, Page.CmsCategoryExtended) && (
           <Route exact path={`${match.path}/cms-category-extended`} component={CategoryExtended} />
         )}
-        )}
+
 
         {/* keep least always */}
         <Route path={`${match.path}/*`}>
