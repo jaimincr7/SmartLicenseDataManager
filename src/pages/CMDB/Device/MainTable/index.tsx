f705fde8db6bde41418937b97732c65e54e7cd07
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-10-14 16:41:12 +0530

    CMDB CRUDS Filter optimization.

--- src/pages/CMDB/Device/MainTable/index.tsx
+++ src/pages/CMDB/Device/MainTable/index.tsx
@@ -1,5 +1,5 @@
 import { Popconfirm } from 'antd';
-import React, { forwardRef, useImperativeHandle, useRef, useEffect } from 'react';
+import React, { forwardRef, useImperativeHandle, useRef, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../store/app.hooks';
 import moment from 'moment';
 import { Common } from '../../../../common/constants/common';
@@ -28,6 +28,7 @@ const MainTable: React.ForwardRefRenderFunction<unknown, IMainTable> = (props, r
   const dispatch = useAppDispatch();
   const dataTableRef = useRef(null);
   const history = useHistory();
+  const [ObjectForColumnFilter, setObjectForColumnFilter] = useState({});
 
   useImperativeHandle(ref, () => ({
     refreshData() {
@@ -46,7 +47,17 @@ const MainTable: React.ForwardRefRenderFunction<unknown, IMainTable> = (props, r
   };
 
   const FilterBySwap = (dataIndex: string, form) => {
-    return FilterWithSwapOption(dataIndex, cmdbDevice.search.tableName, form);
+    return FilterWithSwapOption(
+      dataIndex,
+      cmdbDevice.search.tableName,
+      form,
+      null,
+      ObjectForColumnFilter
+    );
+  };
+
+  const FilterByDateSwapTable = (dataIndex: string, tableName: string, form: any) => {
+    return FilterByDateSwap(dataIndex, tableName, form, null, ObjectForColumnFilter);
   };
 
   const getTableColumns = (form) => {
@@ -168,7 +179,7 @@ const MainTable: React.ForwardRefRenderFunction<unknown, IMainTable> = (props, r
         sorter: true,
         children: [
           {
-            title: FilterByDateSwap('last_updated', cmdbDevice.search.tableName, form),
+            title: FilterByDateSwapTable('last_updated', cmdbDevice.search.tableName, form),
             dataIndex: 'last_updated',
             key: 'last_updated',
             ellipsis: true,
@@ -409,6 +420,7 @@ const MainTable: React.ForwardRefRenderFunction<unknown, IMainTable> = (props, r
         setShowSelectedListModal={setShowSelectedListModal}
         setValuesForSelection={setValuesForSelection}
         showBulkUpdate={ability.can(Action.Update, Page.CmdbDevice)}
+        setObjectForColumnFilter={setObjectForColumnFilter}
       />
     </>
   );
