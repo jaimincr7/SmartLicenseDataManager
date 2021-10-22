4c25d1b64191340033bcc037985c79c69bc69549
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-10-22 15:02:12 +0530

    RVTools Pre-Select Global Filters and Date Added.

--- src/pages/RVTools/TabVCluster/AddTabVClusterModal/index.tsx
+++ src/pages/RVTools/TabVCluster/AddTabVClusterModal/index.tsx
@@ -18,6 +18,7 @@ import BreadCrumbs from '../../../../common/components/Breadcrumbs';
 import { validateMessages } from '../../../../common/constants/common';
 import { Page } from '../../../../common/constants/pageAction';
 import { getObjectForUpdateMultiple } from '../../../../common/helperFunction';
+import { IInlineSearch } from '../../../../common/models/common';
 import { ILookup } from '../../../../services/common/common.model';
 import { ITabVCluster } from '../../../../services/rvTools/tabVCluster/tabVCluster.model';
 import { useAppSelector, useAppDispatch } from '../../../../store/app.hooks';
@@ -33,6 +34,7 @@ import {
   clearMultipleUpdateMessages,
   commonSelector,
 } from '../../../../store/common/common.reducer';
+import { globalSearchSelector } from '../../../../store/globalSearch/globalSearch.reducer';
 import {
   getTabVClusterById,
   saveTabVCluster,
@@ -50,6 +52,7 @@ const AddTabVClusterModal: React.FC<IAddTabVClusterProps> = (props) => {
   const tabVCluster = useAppSelector(tabVClusterSelector);
   const commonLookups = useAppSelector(commonSelector);
   const dispatch = useAppDispatch();
+  const globalFilters = useAppSelector(globalSearchSelector);
 
   const { id, showModal, handleModalClose, refreshDataTable, isMultiple, valuesForSelection } =
     props;
@@ -217,6 +220,21 @@ const AddTabVClusterModal: React.FC<IAddTabVClusterProps> = (props) => {
     };
   }, [dispatch]);
 
+  useEffect(() => {
+    if(!isMultiple) {
+        const globalSearch: IInlineSearch = {};
+        for (const key in globalFilters.search) {
+          const element = globalFilters.search[key];
+          globalSearch[key] = element ? [element] : null;
+        }
+        if (globalSearch.company_id) {
+          dispatch(getCompanyLookup(globalSearch.tenant_id[0]));
+          dispatch(getBULookup(globalSearch.company_id[0]));
+        }
+        form.setFieldsValue(globalSearch);
+      }
+      }, []);
+      
   return (
     <>
       <Modal
