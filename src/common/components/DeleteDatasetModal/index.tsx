168f79aa0278ca6aafade64561be6396abb0f357
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-10-29 14:29:23 +0530

    Global Filters issue solved for all Delete Dataset.

--- src/common/components/DeleteDatasetModal/index.tsx
+++ src/common/components/DeleteDatasetModal/index.tsx
@@ -93,18 +93,18 @@ const DeleteDatasetModal: React.FC<IDeleteDatasetModalProps> = (props) => {
       const element = globalFilters.search[key];
       globalSearch[key] = element ? [element] : null;
     }
-    if (globalSearch.company_id) {
-      dispatch(getBULookup(globalSearch.company_id[0]));
+    if (globalSearch.company_id && globalSearch.bu_id) {
+      dispatch(getBULookup(globalSearch?.company_id[0]));
+      initialValues = {
+        company_id: _.isNull(globalSearch.company_id) ? null : globalSearch?.company_id[0],
+        bu_id: _.isNull(globalSearch.bu_id) ? null : globalSearch?.bu_id[0],
+        date_added:
+          filterKeys?.filter_keys?.date_added?.length == 1
+            ? moment(filterKeys.filter_keys.date_added[0]).format(Common.DATEFORMAT)
+            : null,
+      };
+      form.setFieldsValue(initialValues);
     }
-    initialValues = {
-      company_id: _.isNull(globalSearch.company_id) ? null : globalSearch.company_id[0],
-      bu_id: _.isNull(globalSearch.bu_id) ? null : globalSearch.bu_id[0],
-      date_added:
-        filterKeys?.filter_keys?.date_added?.length == 1
-          ? moment(filterKeys.filter_keys.date_added[0]).format(Common.DATEFORMAT)
-          : null,
-    };
-    form.setFieldsValue(initialValues);
   }, []);
 
   return (
