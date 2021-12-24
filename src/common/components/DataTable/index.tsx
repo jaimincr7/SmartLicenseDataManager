318bc421b0431b4d21a2ab4c080731a4c709c07b
Rohit Goyani <rohit.goyani@tatvasoft.com>
2021-12-24 14:28:30 +0530

    reset back issue fixed.

--- src/common/components/DataTable/index.tsx
+++ src/common/components/DataTable/index.tsx
@@ -295,14 +295,14 @@ const DataTable: React.ForwardRefRenderFunction<unknown, IDataTable> = (props, r
       const element = globalFilters.search[key];
       globalSearch[key] = element ? [element] : null;
     }
-    if (globalFilters.search.tenant_id && globalFilters.search.tenant_id !== 0) {
+    if (globalFilters.search.tenant_id && globalFilters.search.tenant_id !== 0 && !(globalSearchExist==false)) {
       const initlValues = {
         company_id: _.isNull(globalSearch.company_id) ? null : globalSearch.company_id[0],
         bu_id: _.isNull(globalSearch.bu_id) ? null : globalSearch.bu_id[0],
         tenant_id: _.isNull(globalSearch.tenant_id) ? null : globalSearch.tenant_id[0],
       };
       form.setFieldsValue(initlValues);
-      if (globalFilters.search && !(globalSearchExist == false)) {
+      if (globalFilters.search) {
         onFinish(initlValues);
       }
     } else {
