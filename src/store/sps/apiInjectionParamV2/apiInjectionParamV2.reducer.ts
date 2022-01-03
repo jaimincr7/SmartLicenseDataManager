2792c39ace10431ad87f079e154c95fc619db3ce
Rohit Goyani <rohit.goyani@tatvasoft.com>
2022-01-03 16:27:25 +0530

    Change in SPSAPI INJECTION PARAM SEARCH RESPONSE.

--- src/store/sps/apiInjectionParamV2/apiInjectionParamV2.reducer.ts
+++ src/store/sps/apiInjectionParamV2/apiInjectionParamV2.reducer.ts
@@ -72,18 +72,18 @@ export const spsApiInjectionParamV2Slice = createSlice({
       state.search.loading = true;
     },
     [searchSpsApiInjectionParamV2.fulfilled.type]: (state, action: PayloadAction<any>) => {
-      const { records, total_count, table_name, column_selection, ...rest } = action.payload;
-      state.search.data = records;
-      state.search.count = total_count;
+      const { search_result, ...rest } = action.payload;
+      state.search.data = search_result.records;
+      state.search.count = search_result.total_count;
       if (JSON.stringify(rest) !== '{}') {
         state.search.lookups = { ...rest, booleanLookup };
       }
       state.search.loading = false;
       state.search.hasErrors = false;
-      state.search.tableName = table_name;
-      if (column_selection) {
-        state.tableColumnSelection.id = column_selection.id;
-        const tableSelectionObj = JSON.parse(column_selection.columns as any);
+      state.search.tableName = search_result.table_name;
+      if (search_result.column_selection) {
+        state.tableColumnSelection.id = search_result.column_selection.id;
+        const tableSelectionObj = JSON.parse(search_result.column_selection.columns as any);
         if (tableSelectionObj.columns) {
           state.tableColumnSelection.column_orders = tableSelectionObj.column_orders;
           state.tableColumnSelection.columns = tableSelectionObj.columns;
@@ -91,7 +91,7 @@ export const spsApiInjectionParamV2Slice = createSlice({
           state.tableColumnSelection.columns = tableSelectionObj;
         }
       }
-      state.tableColumnSelection.table_name = table_name;
+      state.tableColumnSelection.table_name = search_result.table_name;
     },
     [searchSpsApiInjectionParamV2.rejected.type]: (state) => {
       state.search.loading = false;
