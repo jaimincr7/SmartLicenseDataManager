5a16b9f7d6b522dcc1544f3ff43b21cbac4338b0
Rohit Goyani <rohit.goyani@tatvasoft.com>
2022-01-04 15:48:25 +0530

    Set Time Out for retry  Sps Api  Job Data

--- src/services/sps/spsApiJobsData/spsApiJobsData.service.ts
+++ src/services/sps/spsApiJobsData/spsApiJobsData.service.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import { IApiResponse, ISearchResponse } from '../../../common/models/common';
 import request from '../../../utils/request';
 import { ICallAPI, ISearchSpsApiJobsData, ISpsApiJobsData } from './spsApiJobsData.model';
@@ -16,8 +18,29 @@ class SpsApiJobsDataService {
 
   public async runJobData(data: ICallAPI): Promise<any> {
     const url = `${this.ENDPOINT}/run-job-data`;
-    return request({ url, method: 'POST', data: data }).then((res) => {
-      return res.data;
+    const cancelTokenSource = axios.CancelToken.source();
+
+    return new Promise((resolve, reject) => {
+      const timmer = setTimeout(() => {
+        // Cancel request
+        cancelTokenSource.cancel();
+        toast.warning('Process is working in background.');
+        reject();
+      }, 30 * 1000); // wait till 30 seconds
+
+      request({ url, method: 'POST', data: data, cancelToken: cancelTokenSource.token })
+        .then((res) => {
+          return res?.data;
+        })
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((data) => {
+          reject(data);
+        })
+        .finally(() => {
+          clearTimeout(timmer);
+        });
     });
   }
 
