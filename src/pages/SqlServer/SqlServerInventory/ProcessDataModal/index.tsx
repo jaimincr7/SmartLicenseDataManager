bb1b7901d678ebd405c16a9ef336a6baefb27292
Rohit Goyani <rohit.goyani@tatvasoft.com>
2022-01-19 19:20:36 +0530

    Sql Server Inventory Process Data issue fixed.

--- src/pages/SqlServer/SqlServerInventory/ProcessDataModal/index.tsx
+++ src/pages/SqlServer/SqlServerInventory/ProcessDataModal/index.tsx
@@ -180,7 +180,7 @@ const ProcessDataModal: React.FC<IProcessDataModalProps> = (props) => {
       dispatch(getBULookup(globalSearch.company_id[0]));
       const filterValues = {
         company_id: _.isNull(globalSearch.company_id) ? null : globalSearch.company_id[0],
-        bu_id: _.isNull(globalSearch.bu_id) && globalSearch.bu_id ? null : globalSearch.bu_id[0],
+        bu_id: _.isNull(globalSearch.bu_id) || !(globalSearch.bu_id) ? null : globalSearch.bu_id[0],
         date_added:
           filterKeys?.filter_keys?.date_added?.length === 1
             ? moment(filterKeys.filter_keys.date_added[0]).format(Common.DATEFORMAT)
