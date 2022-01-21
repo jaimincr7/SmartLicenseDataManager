dae774a802f89c33bdfa49726e973db4f98fc6c4
Rohit Goyani <rohit.goyani@tatvasoft.com>
2022-01-21 18:40:18 +0530

    Ad User and Delete Data set Modal for configuration.

--- src/pages/Ad/AdUsers/ProcessDataModal/index.tsx
+++ src/pages/Ad/AdUsers/ProcessDataModal/index.tsx
@@ -60,14 +60,23 @@ const ProcessDataModal: React.FC<IProcessDataModalProps> = (props) => {
   };
 
   const saveConfig = () => {
+    const globalSearch: IInlineSearch = {};
+    for (const key in globalFilters.search) {
+      const element = globalFilters.search[key];
+      globalSearch[key] = element ? [element] : null;
+    }
+    const fieldValues = { ...form.getFieldsValue() };
+    delete fieldValues.date_added;
     const setModelSelection: IConfigModelPopUpDataSelection = {
       id:
         commonLookups.getModelPopUpSelection.id === null
           ? null
           : commonLookups.getModelPopUpSelection.id,
-      selection: JSON.stringify(form.getFieldsValue()),
+      selection: JSON.stringify(fieldValues),
       table_name: tableName,
       pop_up_name: 'ProcessDataSet',
+      company_id: form.getFieldValue('company_id'),
+      bu_id: form.getFieldValue('bu_id'),
     };
     dispatch(configModelPopUpDataSelection(setModelSelection));
   };
@@ -151,17 +160,21 @@ const ProcessDataModal: React.FC<IProcessDataModalProps> = (props) => {
   }, [commonLookups.setModelPopUpSelection.messages]);
 
   React.useEffect(() => {
-    if (ability.can(Action.ModelDataSeletion, Page.ConfigModelPopUpSelection)) {
-      const modelPopUp: IGetConfigModelPopUpDataSelection = {
-        table_name: tableName,
-        pop_up_name: 'ProcessDataSet',
-      };
-      dispatch(getConfigModelPopUpDataSelection(modelPopUp));
-    }
     const globalSearch: IInlineSearch = {};
     for (const key in globalFilters.search) {
       const element = globalFilters.search[key];
       globalSearch[key] = element ? [element] : null;
+    }
+    if (ability.can(Action.ModelDataSeletion, Page.ConfigModelPopUpSelection)) {
+      const modelPopUp: IGetConfigModelPopUpDataSelection = {
+        table_name: tableName,
+        pop_up_name: 'ProcessDataSet',
+        tenant_id: _.isNull(globalSearch.tenant_id) || !(globalSearch.tenant_id) ? null : globalSearch.tenant_id[0],
+        company_id: _.isNull(globalSearch.company_id) || !(globalSearch.company_id) ? null : globalSearch.company_id[0],
+        bu_id: _.isNull(globalSearch.bu_id) || !(globalSearch.bu_id) ? null : globalSearch.bu_id[0],
+      };
+      if(globalSearch.company_id && globalSearch.company_id[0] !== 0)
+      dispatch(getConfigModelPopUpDataSelection(modelPopUp));
     }
     if (
       globalFilters.search.company_id ||
@@ -341,6 +354,7 @@ const ProcessDataModal: React.FC<IProcessDataModalProps> = (props) => {
               <Button
                 type="dashed"
                 ghost
+                disabled={form.getFieldValue('company_id') === null}
                 onClick={saveConfig}
                 loading={commonLookups.setModelPopUpSelection.loading}
               >
