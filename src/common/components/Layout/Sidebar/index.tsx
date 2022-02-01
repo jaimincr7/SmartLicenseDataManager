4c17141f2b48235382e6eac6459c9a6572e713b8
bt101 <30605815+bt101@users.noreply.github.com>
2022-02-01 11:54:20 -0500

    Remove Left Navigation Menu Expand / Collapse
    Removed the Left Navigation Menu Expand / Collapse as it doesn't work when 2nd level menu items are parents.  I only updated this one file.  I tried to create a branch and make the change in there and merge it back but it looks like it would change ALL files.

--- src/common/components/Layout/Sidebar/index.tsx
+++ src/common/components/Layout/Sidebar/index.tsx
@@ -11,16 +11,6 @@ function Sidebar() {
   const location = useLocation();
   const defaultSubmenu: string = location.pathname.split('/')[1];
   const userDetails = useAppSelector(userSelector);
-  const [openKeys, setOpenKeys] = React.useState([`${defaultSubmenu}`]);
-
-  const onOpenChange = keys => {
-    const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
-    if (keys.indexOf(latestOpenKey) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
 
   window.addEventListener('click', function (e) {
     if (
@@ -69,8 +59,7 @@ function Sidebar() {
       <Scrollbars renderThumbVertical={(props) => <div {...props} className="track-vartical" />}>
         <Menu
           // defaultSelectedKeys={['/']}
-          openKeys={openKeys}
-          onOpenChange={onOpenChange}
+          defaultOpenKeys={[`${defaultSubmenu}`]}
           mode="inline"
           selectedKeys={[location.pathname]}
         >
