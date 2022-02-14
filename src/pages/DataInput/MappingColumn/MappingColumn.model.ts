25c7a418e9538949318c446b0e678cb934d8b4fe
Rohit Goyani <rohit.goyani@tatvasoft.com>
2022-02-14 17:42:17 +0530

    1st commit in Date Added Bulk Import.

--- src/pages/DataInput/MappingColumn/MappingColumn.model.ts
+++ src/pages/DataInput/MappingColumn/MappingColumn.model.ts
@@ -9,4 +9,6 @@ export interface IMappingColumnProps {
   is_public?: boolean;
   setRecords?: (data: any) => void;
   onExcelMapping?: (values: any) => void;
+  primaryDate?: any;
+  selectedDate?: number;
 }
